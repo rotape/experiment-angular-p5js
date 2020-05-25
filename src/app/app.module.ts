56c8a987e60c02a9f22ee02bf19b252ad0fa1c1b
rotape <roger.correu@gmail.com>
2020-05-25 20:05:37 +0200

    remove unused packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,26 +14,14 @@ import { MatMenuModule } from "@angular/material/menu";
 import { MatSelectModule } from "@angular/material/select";
 
 import { AppComponent } from "./app.component";
-import { ExemplePrimerComponent } from "./exemple-primer/exemple-primer.component";
-import "p5";
 import { FetchPipe } from "./pipes/fetch.pipe";
-import { ExempleSegonComponent } from "./exemple-segon/exemple-segon.component";
 import { WebAudioComponentComponent } from "./web-audio-component/web-audio-component.component";
-import { CommonModule } from "@angular/common";
 const appRoutes: Routes = [
-  { path: "template1", component: ExemplePrimerComponent },
-  { path: "template2", component: ExempleSegonComponent },
   { path: "audioComponent", component: WebAudioComponentComponent },
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExemplePrimerComponent,
-    FetchPipe,
-    ExempleSegonComponent,
-    WebAudioComponentComponent,
-  ],
+  declarations: [AppComponent, FetchPipe, WebAudioComponentComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
